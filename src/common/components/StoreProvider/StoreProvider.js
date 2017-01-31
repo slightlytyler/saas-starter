9e8dabe6ea117ca3c9d66d2cc3a976c23519f5ef
Tyler Martinez <slightlytyler@gmail.com>
2017-01-31 13:54:45 -0600

    Remove withStorage container

--- src/common/components/StoreProvider/StoreProvider.js
+++ src/common/components/StoreProvider/StoreProvider.js
@@ -1,6 +1,11 @@
-import withStorage from 'common/containers/withStorage';
+import spinnerWhileLoading from 'common/containers/spinnerWhileLoading';
+import withActions from 'common/containers/withActions';
+import { compose, get } from 'lodash/fp';
+import { registerToken } from 'modules/auth/actions';
+import { selectIsAuthenticated, selectToken } from 'modules/auth/selectors';
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
+import { lifecycle, withState } from 'recompose';
 
 const StoreProvider = ({ children, store }) => (
   <Provider store={store}>
@@ -13,4 +18,20 @@ StoreProvider.propTypes = {
   store: PropTypes.object.isRequired,
 };
 
-export default withStorage(StoreProvider);
+const container = compose(
+  withActions({ registerToken }),
+  withState('loading', 'setLoading', true),
+  lifecycle({
+    async componentDidMount() {
+      await this.props.store.loadStorage();
+      const state = this.props.store.getState();
+      if (selectIsAuthenticated(state)) {
+        this.props.registerToken({ token: selectToken(state) });
+      }
+      this.props.setLoading(false);
+    },
+  }),
+  compose(spinnerWhileLoading, get)('loading'),
+);
+
+export default container(StoreProvider);
