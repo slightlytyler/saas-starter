4d519ebf7e84cb5cebf6f025949ac219fbd4b383
Tyler Martinez <slightlytyler@gmail.com>
2017-03-19 00:18:00 -0500

    Deploy: Remove test logs from script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,3 @@
-console.log('Does it hang at the start?');
 const exec = require('child_process').exec;
 const path = require('path');
 
@@ -7,18 +6,11 @@ const project = path.join(rootPath, 'dist');
 const domain = process.env.CI_ENVIRONMENT_URL;
 const command = `surge -p ${project} -d ${domain}`
 
-console.log(command);
-
-console.log('Does it hang here?');
-
 exec(command, (error, stdout, stderr) => {
   if (error) {
     console.error(`[DEPLOY ERROR]: ${error}`);
     return;
   }
-  console.log('Inside the callback?');
   console.log(stdout);
   console.log(stderr);
 });
-
-console.log('What about here?');
