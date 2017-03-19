cfb1336205f785cc97a4c03c879697fa133c843a
Tyler Martinez <slightlytyler@gmail.com>
2017-03-18 23:10:39 -0500

    CI: Write deploy script

--- scripts/deploy/index.js
+++ scripts/deploy/index.js
@@ -1,2 +1,7 @@
-console.log('Write a deploy script!');
-console.log(JSON.stringify(process.env));
+const exec = require('child_process').exec;
+const path = require('path');
+
+const project = path.join(__dirname, 'dist');
+const domain = process.env.CI_ENVIRONMENT_URL;
+
+exec(`surge -p ${project} -d ${domain}`);
