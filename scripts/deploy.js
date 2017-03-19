2cd0c44496926cbaa17f9626ab1291aac944928d
Tyler Martinez <slightlytyler@gmail.com>
2017-03-18 23:31:00 -0500

    Deploy: Fix script

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const exec = require('child_process').exec;
+const path = require('path');
+
+const rootPath =  path.join(__dirname, '..')
+const project = path.join(rootPath, 'dist');
+const domain = process.env.CI_ENVIRONMENT_URL;
+const command = `surge -p ${project} -d ${domain}`
+
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`[DEPLOY ERROR]: ${error}`);
+    return;
+  }
+  console.log(stdout);
+  console.log(stderr);
+});
--- scripts/deploy/index.js
+++
@@ -1,7 +0,0 @@
-const exec = require('child_process').exec;
-const path = require('path');
-
-const project = path.join(__dirname, 'dist');
-const domain = process.env.CI_ENVIRONMENT_URL;
-
-exec(`surge -p ${project} -d ${domain}`);
