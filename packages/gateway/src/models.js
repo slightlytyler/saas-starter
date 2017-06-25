3630808bbc7d5bf082bc21dfb34cdd3263bea3f9
Tyler Martinez <slightlytyler@gmail.com>
2017-06-24 23:58:54 -0700

    Cleanup gateway

--- packages/gateway/src/models.js
+++ packages/gateway/src/models.js
@@ -3,20 +3,20 @@ import AuthModel from 'features/auth/model';
 import PostModel from 'features/post/model';
 import UserModel from 'features/user/model';
 
-const postgresConnector = new PostgresConnector({
+const storeConnector = new PostgresConnector({
   client: 'pg',
   connection: {
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    host: process.env.STORE_HOST,
+    user: process.env.STORE_USER,
+    password: process.env.STORE_PASSWORD,
+    database: process.env.STORE_DATABASE,
   },
 });
 
 const models = {
-  auth: new AuthModel(postgresConnector),
-  post: new PostModel(postgresConnector),
-  user: new UserModel(postgresConnector),
+  auth: new AuthModel(storeConnector),
+  post: new PostModel(storeConnector),
+  user: new UserModel(storeConnector),
 };
 
 export default models;
